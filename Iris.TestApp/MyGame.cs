34e2914a3424faf159b07e98a53e79cf4d821ff9
Ciastex <Ciastex@users.noreply.github.com>
2020-02-23 23:58:25 +0100

    Add SDL2 binaries and a way to load them from custom location.

--- Iris.TestApp/MyGame.cs
+++ Iris.TestApp/MyGame.cs
@@ -5,10 +5,6 @@ namespace Iris.TestApp
 {
     public class MyGame : Game
     {
-        private Sprite _sprite;
-        private Sprite _sprite2;
-        private Sprite _sprite3;
-
         private int HorizDirection = 1;
         private int VertDirection = 1;
 
@@ -25,17 +21,11 @@ namespace Iris.TestApp
 
         protected override void LoadContent()
         {
-            _sprite = new Sprite("E:\\dvd.png") { Color = Colors.Lime, Scale = 0.5f, ScalingMode = SpriteScalingMode.NearestNeighbor };
-            _sprite2 = new Sprite("E:\\dvd.png") { Color = Colors.Lime, Scale = 0.5f, ScalingMode = SpriteScalingMode.Linear };
-            _sprite3 = new Sprite("E:\\dvd.png") { Color = Colors.Lime, Scale = 0.5f, ScalingMode = SpriteScalingMode.Anisotropic };
         }
 
         protected override void Draw(RenderContext context)
         {
-            //context.FillRectangle(X, Y, Width, Height, new Color(0, 255, 0));
-            _sprite.Draw(context);
-            _sprite2.Draw(context);
-            _sprite3.Draw(context);
+            context.FillRectangle(X, Y, 32, 32, new Color(0, 255, 0));
         }
 
         protected override void Update(double deltaTime)
@@ -44,24 +34,17 @@ namespace Iris.TestApp
 
             if (X - 1 <= 0)
                 HorizDirection = 1;
-            else if (X + _sprite.ActualWidth >= Window.Renderer.Viewport.Value.Size.Width)
+            else if (X + 32 >= Window.Renderer.Viewport.Value.Size.Width)
                 HorizDirection = -1;
 
             if (Y - 1 <= 0)
                 VertDirection = 1;
-            else if (Y + _sprite.ActualHeight >= Window.Renderer.Viewport.Value.Size.Height)
+            else if (Y + 32 >= Window.Renderer.Viewport.Value.Size.Height)
                 VertDirection = -1;
 
             X += 0 * HorizDirection;
             Y += 0 * VertDirection;
 
-            _sprite.X = X;
-            _sprite.Y = Y;
-            _sprite2.Y = Y;
-            _sprite3.Y = Y;
-
-            _sprite2.X = _sprite.X + _sprite.ActualWidth + 32;
-            _sprite3.X = _sprite2.X + _sprite2.ActualWidth + 32;
         }
     }
 }
---
+++ Iris/ModuleInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Iris
+{
+    public static class ModuleInitializer
+    {
+        public static void Initialize()
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                var localPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var dllPath = Path.Combine(localPath, "SdlBinaries");
+
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    dllPath = Path.Combine(dllPath, "Win64");
+                }
+                else
+                {
+                    dllPath = Path.Combine(dllPath, "Win32");
+                }
+
+                var env = Environment.GetEnvironmentVariable("PATH");
+                env = $"{dllPath};{env}";
+
+                Environment.SetEnvironmentVariable("PATH", env);
+            }
+        }
+    }
+}
