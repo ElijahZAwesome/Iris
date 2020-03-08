adbb045c03042b105d2c42d84a743a31873da7e1
Ciastex <mrciastexx@gmail.com>
2020-03-08 09:24:09 +0100

    Add vectors for size and position

--- Iris/Rectangle.cs
+++ Iris/Rectangle.cs
@@ -9,12 +9,40 @@ namespace Iris
         public float Width;
         public float Height;
 
+        public Vector2 Position
+        {
+            get => new Vector2(Left, Top);
+            set
+            {
+                Left = value.X;
+                Top = value.Y;
+            }
+        }
+
+        public Vector2 Size
+        {
+            get => new Vector2(Width, Height);
+            set
+            {
+                Width = value.X;
+                Height = value.Y;
+            }
+        }
+
         public Rectangle(float left, float top, float width, float height)
         {
             Left = left;
             Top = top;
             Width = width;
             Height = height;
+        }
+
+        public Rectangle(Vector2 position, Vector2 size)
+        {
+            Left = position.X;
+            Top = position.Y;
+            Width = size.X;
+            Height = size.Y;
         }
 
         public bool Intersects(Rectangle other)
@@ -24,7 +52,7 @@ namespace Iris
                    Top + Height >= other.Top &&
                    Top <= other.Top + other.Height;
         }
-        
+
         internal IntRect ToSfmlIntRect()
             => new IntRect((int)Left, (int)Top, (int)Width, (int)Height);
     }
